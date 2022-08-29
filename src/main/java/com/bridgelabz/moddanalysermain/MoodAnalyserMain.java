ac8ac43cd7b77f077496e66f025efa3715644f24
hp-pc <navyashree811@gmail.com>
2022-08-29 17:16:10 +0530

    Added : Welcome to Mood Analyser Program

---
+++ src/main/java/com/bridgelabz/moddanalysermain/MoodAnalyserMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.moddanalysermain;
+
+public class MoodAnalyserMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Mood Analyser Progrm");
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/moddanalysertest/MoodAnalyserTest.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.moddanalysertest;
+
+public class MoodAnalyserTest {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Mood Analyser Progrm");
+	}
+
+}
