949ef80b1ee4dccd378e81bb0b79133ef518b5b6
hp-pc <navyashree811@gmail.com>
2022-08-29 18:58:47 +0530

    Added : Given a Message ability to analyse and respond Happy or Sad Mood

--- src/main/java/com/bridgelabz/moddanalysermain/MoodAnalyserMain.java
+++ src/main/java/com/bridgelabz/moddanalysermain/MoodAnalyserMain.java
@@ -1,8 +1,25 @@
 package com.bridgelabz.moddanalysermain;
 
 public class MoodAnalyserMain {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Mood Analyser Progrm");
+	private String message;
+
+	public MoodAnalyserMain(String message) {
+		this.message = message;
 	}
 
+	public String moodHappy() {
+		if (message.contains("happy")) {
+			return "HAPPY";
+		} else {
+			return "SAD";
+		}
+	}
+
+	public String moodSad() {
+		if (message.contains("sad")) {
+			return "SAD";
+		} else {
+			return "HAPPY";
+		}
+	}
 }
--- src/test/java/com/bridgelabz/moddanalysertest/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moddanalysertest/MoodAnalyserTest.java
@@ -1,8 +1,27 @@
 package com.bridgelabz.moddanalysertest;
 
+import org.junit.Test;
+
+import com.bridgelabz.moddanalysermain.MoodAnalyserMain;
+
+import junit.framework.Assert;
+
 public class MoodAnalyserTest {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Mood Analyser Progrm");
+
+	@Test
+	public void givenMoodShouldReturnHappy() {
+		MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("happy");
+		String mood = moodAnalyser.moodHappy();
+		Assert.assertEquals("HAPPY", mood);
+
+	}
+
+	@Test
+	public void givenMoodShouldReturnSad() {
+		MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("sad");
+		String mood = moodAnalyser.moodSad();
+		Assert.assertEquals("SAD", mood);
+
 	}
 
 }
